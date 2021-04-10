afa969adddae25285a7e5234035008a00aa6aeb7
olliehughes987 <p2421386@my365.dmu.ac.uk>
2021-04-10 14:09:43 +0100

    started routine page

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 menuToggler.addEventListener('click', event => {
   menu.classList.toggle('menu--open');
 });
+
+addText.addEventListener('click', event => {
+  var input = document.createElement('input');
+  routineGrid.appendChild(input); 
+  console.log("Text box added");
+});
