e72248da5815d6dad98be95d2b95f1054abf8c82
olliehughes987 <p2421386@my365.dmu.ac.uk>
2021-04-11 18:42:04 +0100

    minor tweaks to routine page

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,31 @@
 var count = 1;
 disableButton("delText");
 
+
+/*  ----FUNCTIONS----  */
+
+  // disables/enables the desired buttons
+function disableButton(elem) {
+  document.getElementById(elem).disabled = true;
+}
+
+function enableButton(elem) {
+  document.getElementById(elem).disabled = false;
+}
+
+/* ----ON CLICK---- */
+
+/* When the hamburger menu button is clicked, it sets the state of the menuToggle
+   to menu--open which allows for event management
+*/
 menuToggler.addEventListener('click', event => {
   menu.classList.toggle('menu--open');
 });
 
+/* When the addText button (+) is clicked, it adds a text box under the current ones.
+   However, if there is already 7 then is disables the button as it has reached the
+   limit.
+*/
 addText.addEventListener('click', event => {
   switch(count) {
     case 7:
@@ -17,7 +38,11 @@ addText.addEventListener('click', event => {
       routineGrid.appendChild(input);
   }});
 
-
+/* Performs the same as the addText but removes instead of creating. Once there is
+   only 2 elements left in the list and the button is clicked, it disables the delete
+   button as otherwise the user could click it and make the variable go negative which
+   would cause an error.
+*/
 delText.addEventListener('click', event => {
   switch(count) {
     case 2:
@@ -28,13 +53,3 @@ delText.addEventListener('click', event => {
       count = count - 1;
       enableButton("addText");
   }});
-
-
-
-function disableButton(elem) {
-  document.getElementById(elem).disabled = true;
-}
-
-function enableButton(elem) {
-  document.getElementById(elem).disabled = false;
-}
