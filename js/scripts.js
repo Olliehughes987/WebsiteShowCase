d0971135205d586b898be3d49494fa5f2e5aaf64
olliehughes987 <p2421386@my365.dmu.ac.uk>
2021-04-11 18:23:28 +0100

    routine add/delete buttons now work

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,40 @@
-var count = 0;
+var count = 1;
+disableButton("delText");
 
 menuToggler.addEventListener('click', event => {
   menu.classList.toggle('menu--open');
 });
 
 addText.addEventListener('click', event => {
-  var input = document.createElement('input');
-  var count = count + 1;
-  console.log(count);
-  routineGrid.appendChild(input);
-  console.log("Text box added");
-});
+  switch(count) {
+    case 7:
+      disableButton("addText");
+    default:
+      enableButton("delText");
+      count = count + 1;
+      var input = document.createElement('input');
+      input.setAttribute("id", "row" + count);
+      routineGrid.appendChild(input);
+  }});
+
+
+delText.addEventListener('click', event => {
+  switch(count) {
+    case 2:
+      disableButton("delText");
+    default:
+      var curr = document.getElementById("row" + count);
+      curr.remove();
+      count = count - 1;
+      enableButton("addText");
+  }});
+
+
+
+function disableButton(elem) {
+  document.getElementById(elem).disabled = true;
+}
+
+function enableButton(elem) {
+  document.getElementById(elem).disabled = false;
+}
