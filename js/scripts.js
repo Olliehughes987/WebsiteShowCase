8b37d4dcbc113df3a43d13a50e4d3bae922b9c1b
olliehughes987 <p2421386@my365.dmu.ac.uk>
2021-04-11 15:38:40 +0100

    rudimentary routine page

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
+var count = 0;
+
 menuToggler.addEventListener('click', event => {
   menu.classList.toggle('menu--open');
 });
 
 addText.addEventListener('click', event => {
   var input = document.createElement('input');
-  routineGrid.appendChild(input); 
+  var count = count + 1;
+  console.log(count);
+  routineGrid.appendChild(input);
   console.log("Text box added");
 });
